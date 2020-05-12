2a855c500b84b3e3032d0b44528ddae3c4df59dd
Neeraj Negi <neeraj.negi@admins-MacBook-Pro.local>
2020-05-12 16:18:32 +0530

    temp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as FormReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: FormReducer
 });
